561425e6f09dd0f4455edf6526f83853141914ef
M Haikal <haikalhaiza1506@gmail.com>
2025-11-02 21:44:37 +0700

    Day 2

--- DAY2/read_image_local.py
+++ DAY2/read_image_local.py
@@ -15,23 +15,20 @@ def decode_image(image_path):
         return base64.b64encode(file.read()).decode()
 
 image_url='../asset/image.png'
+
+base64_image=decode_image(image_url)
+
+
+response=client.chat.completions.create(
+    model='gpt-4o',
+    messages=[
+        {'role':'user','content':[
+            {'type':'text','text':'Can you please describe this image?'},
+            {'type':'image_url','image_url':{'url':f'data:image/png;base64,{base64_image}'}}
+        ]}
+    ]
+
     
+)
 
-
-# image_url='https://imageio.forbes.com/specials-images/imageserve/664b9c049314ec4607e8ee46/Porsche-Sonderwunsch-Haus--The-Taycan-4S-Cross-Turismo-For-Jennie-Ruby-Jane---/0x0.jpg?format=jpg&crop=2432,1824,x0,y291,safe&width=960'
-
-# response=client.chat.completions.create(
-#     model='gpt-4o',
-#     messages=[
-#         {'role':'user','content':[
-#             {'type':'text','text':'Can you please describe this image?'},
-#             {'type':'image_url','image_url':{'url':image_url}}
-#         ]}
-#     ]
-
-    
-# )
-
-# print(response.choices[0].message.content)
-
-print(decode_image(image_url))
+print(response.choices[0].message.content)
